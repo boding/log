a88faf2f098b12859cc95f16bbbc70d83d61b478
boding <bobding@foxmail.com>
2014-09-24 13:40:09 +0800

    add

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Log.h"
+
+int main()
+{
+    LogCritical("Hello World!\n");
+    LogError("Hello World!\n");
+    LogWarn("Hello World!\n");
+    LogPrompt("Hello World!\n");
+
+    return 0;
+}
